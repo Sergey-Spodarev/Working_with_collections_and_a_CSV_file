5fc5f20fbc29dcfc8231b4864e2821188f1b0e31
Sergey-Spodarev <spodarev8@gmail.com>
2025-03-12 12:43:44 +0300

    The classes Humen and Department were created and described namely, they were created by Get Set and toString

---
+++ src/main/java/model/Department.java
@@ -0,0 +1,30 @@
+package model;
+
+public class Department {
+    private int ID;
+    private String name;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "ID=" + ID +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/model/Humen.java
@@ -0,0 +1,72 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Humen {
+    private int ID;
+    private String name;
+    private String gender;
+    private Department department;
+    private double salary;
+    private LocalDate birthDate;
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Humen{" +
+                "ID=" + ID +
+                ", name='" + name + '\'' +
+                ", gender='" + gender + '\'' +
+                ", department=" + department +
+                ", salary=" + salary +
+                ", birthDate=" + birthDate +
+                '}';
+    }
+}
